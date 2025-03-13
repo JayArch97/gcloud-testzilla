be87651ad072746cc3d5754005e1109ebc5afe23
Jayarch97 <jayquintanilla2@gmail.com>
2025-03-13 01:25:13 +0000

    changing tsx to jsx for the frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,76 +1,69 @@
-import { useEffect, useState,ReactNode } from 'react'
-import { createRoot } from 'react-dom/client'
-import axios from 'axios'
-import './index.css'
+import { useEffect, useState, ReactNode } from 'react';
+import { createRoot } from 'react-dom/client';
+import axios from 'axios';
+import './index.css';
+import { MDBContainer, MDBRadio, MDBRow } from 'mdb-react-ui-kit';
 
+const root = createRoot(document.getElementById('root'));
 
-const root = createRoot(document.getElementById('root'))
-
-
-function Test(){
-  const [questions, setQuestions] = useState<{
-    opt_a: ReactNode,
-    opt_b: ReactNode,
-    opt_c: ReactNode,
-    opt_d: ReactNode, questions: string 
-}[]>([])
-
+function Test() {
+  const [questions, setQuestions] = useState([]);
 
   useEffect(() => {
-    getQuestions().then(data=> setQuestions(data))
-  }, [])
+    getQuestions().then((data) => setQuestions(data));
+  }, []);
 
-  const getQuestions =  () => {
-    return axios.get('https://acostajulio-dev.wl.r.appspot.com').then(response => {
-      console.log(response.data)
-      return response.data
-    }).catch(error => {
-      console.log(error)
-    })
-  }
+  const getQuestions = () => {
+    return axios
+      .get('https://acostajulio-dev.wl.r.appspot.com')
+      .then((response) => {
+        console.log(response.data);
+        return response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <>
-  <MDBContainer>
-  <MDBRow className="justify-content-center">
-      <form>
-        {questions.map((question)=>(
-        <>
-            <p> {question.questions}</p>
-            <MDBRadio
-                      name="flexRadioDefault"
-                      id="flexRadioDefault1"
-                      label={question.opt_a}
-                      defaultChecked
-            />
-                 <MDBRadio
-                      name="flexRadioDefault"
-                      id="flexRadioDefault2"
-                      label={question.opt_b}
-            />
-                 <MDBRadio
-                      name="flexRadioDefault"
-                      id="flexRadioDefault3"
-                      label={question.opt_c}
-            />
-                 <MDBRadio
-                      name="flexRadioDefault"
-                      id="flexRadioDefault4"
-                      label={question.opt_d}
-            />
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-          </Form.Group>
-        </>
-        ))}
-    </form>
-      </MDBRow>
+      <MDBContainer>
+        <MDBRow className="justify-content-center">
+          <form>
+            {questions.map((question, index) => (
+              <div key={index}>
+                <p>{question.questions}</p>
+                <MDBRadio
+                  name={`flexRadioDefault-${index}`}
+                  id={`flexRadioDefault1-${index}`}
+                  label={question.opt_a}
+                />
+                <MDBRadio
+                  name={`flexRadioDefault-${index}`}
+                  id={`flexRadioDefault2-${index}`}
+                  label={question.opt_b}
+                />
+                <MDBRadio
+                  name={`flexRadioDefault-${index}`}
+                  id={`flexRadioDefault3-${index}`}
+                  label={question.opt_c}
+                />
+                <MDBRadio
+                  name={`flexRadioDefault-${index}`}
+                  id={`flexRadioDefault4-${index}`}
+                  label={question.opt_d}
+                />
+              </div>
+            ))}
+          </form>
+        </MDBRow>
       </MDBContainer>
     </>
-  )
-} 
-
+  );
+}
 
 root.render(
   <>
     <Test />
   </>
-)
+);
