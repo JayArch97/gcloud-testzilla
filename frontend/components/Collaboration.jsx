4efa1599426b8841246be7220919a39a9fa42713
Julio Armando Quintanilla Acosta <jayquintanilla2@gmail.com>
2025-03-16 02:08:43 -0600

    Adding sphere object and hyperlinks to the profile

--- frontend/components/Collaboration.jsx
+++ frontend/components/Collaboration.jsx
@@ -1,10 +1,51 @@
+import { Canvas, useFrame, useLoader } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { useRef } from 'react';
+import { TextureLoader } from 'three';
+
+function RotatingSphere() {
+    const colorMap = useLoader(TextureLoader, 'quasarTexture.png');
+    const sphereRef = useRef();
+
+    useFrame(() => {
+        if (sphereRef.current) {
+            sphereRef.current.rotation.x += 0.001;
+            sphereRef.current.rotation.y += 0.005;
+        }
+    });
+
+    return (
+        <mesh ref={sphereRef}>
+            <sphereGeometry args={[1, 48, 48]} />
+            <meshStandardMaterial map={colorMap} />
+        </mesh>
+    );
+}
 
 export default function Collaboration() {
+    return (
+        <>
+            <h3 className="flex items-center justify-center h-20 text-xl text-white bg-[oklch(0.141_0.005_285.823)]">
+                Want To Collaborate On This Project?
+            </h3>
+            
+            <div className='flex items-center justify-center bg-[oklch(0.141_0.005_285.823)]'>
+            <div className="flex items-center justify-between gap-4">
+                    {/* h2 on the left */}
+                <a href={'https://github.com/JayArch97/gcloud-testzilla/tree/dev'} className="text-white text-lg hover:text-blue-500">Join Us!</a>
+                <div id="canvas-container" className='w-60 h-80 mt-0.5 bg-[oklch(0.141_0.005_285.823)]'>
+               
+                <Canvas>
+                    <OrbitControls />
+                    <ambientLight intensity={0.1} />
+                    <directionalLight position={[0, 0, 5]} color="red" />
+                    <RotatingSphere />
+                </Canvas>
+            </div>
+            <a href={'https://github.com/JayArch97/gcloud-testzilla/tree/dev'} className="text-white text-lg hover:text-blue-500">Let's Build!</a>
 
-        return (
-            <>
-                <h1>Want To Collaborate On This Project?</h1>
-            </>
-        )
-
+         </div>
+         </div>
+        </>
+    );
 }
--- frontend/components/HomePage.jsx
+++ frontend/components/HomePage.jsx
@@ -8,6 +8,7 @@ import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
 import pdfHomePage from '../src/assets/pdfHomePage.png';
 import supportHomePage from '../src/assets/supportHomePage.png';
+import Collaboration from './Collaboration.jsx';
 
 
 
@@ -18,12 +19,13 @@ export default function PortalExample() {
     Text Color:oklch(0.359_0.144_278.697) */}
 
       <NavBar/>
-      <div className="flex items-center justify-center h-50  bg-[oklch(0.141_0.005_285.823)]">
-        <div className="flex items-center justify-center mr-15 bg-black-100 p-3 rounded-lg hover:text-blue-500">
+
+      <div className="flex items-center justify-center h-50 bg-[oklch(0.141_0.005_285.823)]">
+        <div className="flex items-center justify-center bg-black-100 p-3 rounded-lg hover:text-blue-500">
           <h2 className="w-38 py-2 px-2 border-double text-xl text-white border-5 rounded-lg border-blue-500">Take ACE Quiz</h2>
         </div>
       </div>
-
+      
   <div className="bg-[oklch(0.141_0.005_285.823)] w-full flex space-x items-center justify-center">
     <CardGroup className="gap-4 flex space-x mt-10">  
     <Card style={{ width: '18rem' }} className='mr-20'>
@@ -33,42 +35,42 @@ export default function PortalExample() {
         <Card.Text className="text-md text-white ml-1"> 
           View your performances and track your progress.
         </Card.Text>
-        <Button variant="dark" className="ml-16 text-blue-500 mb-4 mt-2">Go Analytics</Button>
+        <Button variant="dark" className="ml-16 text-blue-500 items-center justify-center mt-2 mb-2">Go Analytics</Button>
       </Card.Body>
       <Card.Footer>
         <small className="text-muted ml-2">Last updated 3 mins ago</small>
       </Card.Footer>
     </Card>
-    <Card style={{ width: '18rem' }} className='mr-20'>
-    <Card.Img src={pdfHomePage} alt="Analytics Home Page" className="h-32 w-39 ml-20" />
+    <Card style={{ width: '18rem' }} className='mr-20 '>
+    <Card.Img src={pdfHomePage} alt="Analytics Home Page" className="h-32 w-39 flex ml-16" />
     <Card.Body>
     <Card.Title className="text-xl text-gray-400 mt-4">Review Questions</Card.Title>
     <Card.Text className="text-md text-white ml-1">
           Don't want to take a Quiz? Just want to review the questions we got you.
         </Card.Text>
-        <Button variant="dark" className="ml-20 text-blue-500 mb-2 mt-4">Go Questions</Button>
+        <Button variant="dark" className="ml-18 text-blue-500 items-center justify-center mt-2 mb-2">Go Questions</Button>
       </Card.Body>
       <Card.Footer className='mb-2'>
       <small className="text-muted ml-2">Last updated 3 mins ago</small>
       </Card.Footer>
     </Card>
     <Card style={{ width: '18rem' }}>
-    <Card.Img src={supportHomePage} alt="Analytics Home Page" className="h-32 w-34 ml-20" />
+    <Card.Img src={supportHomePage} alt="Analytics Home Page" className="h-32 w-34 ml-15" />
     <Card.Body>
     <Card.Title className="text-xl text-gray-400 mt-4">Submit a feature request</Card.Title>
     <Card.Text className="text-md text-white ml-1">
           Didn't find what you were looking for? Submit a feature request.
         </Card.Text>
-        <Button variant="dark" className="ml-20 text-blue-500 mb-2 mt-4">Go Requests</Button>
+        <Button variant="dark" className="ml-16 text-blue-500  items-center justify-center mt-2 mb-2">Go Requests</Button>
       </Card.Body>
-      <Card.Footer className='mb-2'>
+      <Card.Footer className='mb-10'>
       <small className="text-muted ml-2">Last updated 3 mins ago</small>
-      </Card.Footer>
-    </Card>
-  </CardGroup>
-</div>
+          </Card.Footer>
+        </Card>
+      </CardGroup>
+    </div>
 
-
+  <Collaboration/>
 
 
     </>
