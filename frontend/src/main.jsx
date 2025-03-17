323d2a1cd4418d03ac430466e51afe1b77396c6e
Julio Armando Quintanilla Acosta <jayquintanilla2@gmail.com>
2025-03-16 22:15:12 -0600

    adding routing for the homepage and quizpage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,28 @@
 import './index.css';
 import HomePage from '../components/HomePage';
-import {BrowserRouter} from 'react-router-dom';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import QuizPage from '../components/QuizPage';
 
 
+
 const root = createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter(
+  [{
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/quiz',
+    element: <QuizPage />,
+  }
+]
+)
 
 
 root.render(
   <>
-  <BrowserRouter>
-  <HomePage />
-  </BrowserRouter>
+    <RouterProvider router={router} />
   </>
 );
